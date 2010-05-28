bf4f941aec5e281d8917c31ef666e99f727f19b8
Dag Odenhall <dag.odenhall@gmail.com>
2010-05-28 09:07:03 +0200

    Replace the manage script with a shell script

---
+++ shell.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+import readline
+
+from flask import *
+
+from vlasisku import *
+from utils import *
+from db import *
+from models import *
+
+
+os.environ['PYTHONINSPECT'] = 'True'
+
