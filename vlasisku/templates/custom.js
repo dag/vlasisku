926456e832116ba42de52b191a22d50ee615146c
Dag Odenhall <dag.odenhall@gmail.com>
2010-06-01 20:07:30 +0200

    Render javascript with templating for url_for

--- vlasisku/templates/custom.js
+++ vlasisku/templates/custom.js
@@ -1,11 +1,11 @@
 function search() {
-    window.location = '/' + encodeURIComponent(jQuery('#query').attr('value'));
+    window.location = '${url_for('query', query='')}' + encodeURIComponent(jQuery('#query').attr('value'));
     return false;
 }
 
 jQuery(function ($) {
     $('a').tipTip({maxWidth: '600px'});
-    $('#query').autocomplete('/_complete/', {
+    $('#query').autocomplete('${url_for('complete')}', {
         delay: 0,
         matchCase: true,
         selectFirst: false
--- vlasisku/views/general.py
+++ vlasisku/views/general.py
@@ -1,5 +1,6 @@
 
 from flask import send_file
+from flaskext.genshi import render_response
 
 from vlasisku import app
 
@@ -7,4 +8,9 @@ from vlasisku import app
 @app.route('/favicon.ico')
 def favicon():
     return send_file('static/favicon.ico')
+
+
+@app.route('/custom.js')
+def javascript():
+    return render_response('custom.js')
 
