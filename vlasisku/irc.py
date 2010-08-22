a2bf4c73610a0bccbfcce9e8b264dc88b45b7262
Dag Odenhall <dag.odenhall@gmail.com>
2010-08-22 14:31:29 +0200

    Add bots to ##ckule

--- vlasisku/irc.py
+++ vlasisku/irc.py
@@ -52,7 +52,7 @@ class BotBase(IRCClient):
 class FactoryBase(ReconnectingClientFactory):
     server = 'irc.freenode.net'
     port = 6667
-    channels = '#lojban,#jbopre,#vlaski'
+    channels = '#lojban,##ckule,#jbopre,#vlaski'
 
 
 class WordBot(BotBase):
